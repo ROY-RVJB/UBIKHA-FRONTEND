8aaab11a20bd51c79a832e4219f58acf3b057d2c
Roy RVJB <ccorimanya.apaza.roy@gmail.com>
2025-07-13 14:08:16 -0500

    commit prueba

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
       text_1="Pon tu espacio en UBIKHA"
       text_2="Registrate"
       />
-
       <Body
       headline="Encuentra lugares para quedarte en Ubikha"
       description="Ya sea que estés buscando un cuarto,apartamento en un complejo o un castillo, en Ubikha vas a encontrar tu lugar ideal."
