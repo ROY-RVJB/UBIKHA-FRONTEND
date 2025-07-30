f8599a691d550bfd04c1ee4c669796d1b2f441a3
ROY_RVJB <ccorimanya.apaza.roy@gmail.com>
2025-07-29 19:26:59 -0500

    cambios minimos

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,16 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage, LoginPage, HomePageArrendador, MisAnunciosPage } from '../pages';
 import HomePageArrendatario from '../pages/HomePageArrendatario';
-<<<<<<< HEAD
 import PropiedadesAdministradorPage from '../pages/PropiedadesAdministradorPage';
 
-
-
-
-=======
-import UsuariosAdministradorPage from '../pages/UsuariosAdministradorPage';
 import DetallePropiedadPage from '../pages/DetallePropiedadPage';
->>>>>>> fdbe837ea8cad363ea9da273891c18da943384e3
 
 export const AppRouter = () => {
   return (
@@ -23,15 +16,9 @@ export const AppRouter = () => {
         <Route path="/login" element={<LoginPage />} />
 
         {/* 🏠 Dashboards por rol */}
-<<<<<<< HEAD
-        <Route path="/home-arrendador" element={<HomePageArrendador/>}/>
-        <Route path="/home-arrendatario" element={<HomePageArrendatario/>}/>
-        <Route path="/admin-propiedades" element={<PropiedadesAdministradorPage/>} />
-=======
         <Route path="/home-arrendador" element={<HomePageArrendador />} />
         <Route path="/home-arrendatario" element={<HomePageArrendatario />} />
-        <Route path="/admin-dashboard" element={<UsuariosAdministradorPage />} />
->>>>>>> fdbe837ea8cad363ea9da273891c18da943384e3
+        <Route path="/admin-propiedades" element={<PropiedadesAdministradorPage />} />
 
         {/* 🏠 Funcionalidades Arrendador */}
         <Route path="/mis-anuncios" element={<MisAnunciosPage />} />
